ba80ed5558548a01a882b920d14c81f1d009812d
Johongirr <johongir_rahimov@bk.ru>
2022-09-07 11:16:22 +0200

    added url

--- app/server.ts
+++ app/server.ts
@@ -20,8 +20,16 @@ const app = express();
     linkedinStrategy();
     facebookStrategy();
 })()
+
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+});
  
-app.use(cors());
+// app.use(cors());
 // app.use(cors({
 //     origin: "http://localhost:3000",
 //     methods: "GET,POST,PUT,DELETE",
