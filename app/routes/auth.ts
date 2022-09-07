f0011cc40869166c4a8a73ad38bbf08f45051bef
Johongirr <johongir_rahimov@bk.ru>
2022-09-07 16:00:23 +0200

    added with credentials

--- app/routes/auth.ts
+++ app/routes/auth.ts
@@ -1,45 +1,23 @@
-import {Router, Request, Response, NextFunction} from "express"
+import {Router} from "express"
 import passport from "passport"
 import { registerValidator, loginValidator } from "../middlewares/validators";
 import UserController from "../controllers/UserController"
-import User, {UserDocument} from "../models/User"
-import {generateToken} from "../utils";;
 
 const router = Router();
-
-interface AuthProviderData extends UserDocument {
-  authProvider: string;
-}
 
 router.post("/login", loginValidator, passport.authenticate("local"), UserController.loginUser)
 router.post("/register", registerValidator, UserController.registerUser)
 router.get("/login/failed", UserController.loginFailed)
-router.get("/login/success", async (req: Request, res: Response, next: NextFunction)=>{
-  passport.authenticate("local", (err, user, info) => {
-    const currentUser = req.user as AuthProviderData;
-    console.log("Login Success  above if", currentUser)
-    req.logIn(User, err =>{
-        if(err) return next(err);
-        console.log("Login Success User", User)
-        console.log("Login Success currentUser", currentUser)
-        const accessToken = generateToken({id: currentUser?._id, username: currentUser.username}, `${process.env.ACCESS_TOKEN_EXPIRATION_TIME}`)
-        const refreshToken = generateToken({id: currentUser?._id, username: currentUser.username}, `${process.env.REFRESH_TOKEN_EXPIRATION_TIME}`);
-        console.log("Login Success Current Username:", currentUser.username)
-        console.log("Login Success User Token", {id: currentUser._id, accessToken, refreshToken, authProvider: currentUser.authProvider, username: currentUser.username})
-        res.status(200).send({id: currentUser._id, accessToken, refreshToken, authProvider: currentUser.authProvider, username: currentUser.username});
-        
-    })
-  })
-});
+router.get("/login/success", UserController.loginSuccess);
 router.get('/logout', UserController.logoutUser);
 
 // 3rd party authentication
 router.get('/google', passport.authenticate('google', { scope: ['email', 'profile'] }))
 router.get('/google/callback', 
   passport.authenticate('google', {
-    failureRedirect: "/login/failed",
+    failureRedirect: "/login/failed", 
     successRedirect: "https://daily-sleep-tracker.netlify.app/login"
-})),
+}));
 
 router.get('/linkedin',
   passport.authenticate('linkedin', { state: 'SOME STATE'  }));
